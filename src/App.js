337efe38c4edf88d61f610228dd05fff8995fe51
Mc Roniel De Gozo <ronieldegozo@gmail.com>
2022-02-26 05:52:04 +0800

    Re render using map and passing a key id value

--- src/App.js
+++ src/App.js
@@ -3,27 +3,46 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-    constructor(){
-      super();
-      this.state = {
-        string: 'Roniel',
-        age: 23,
-        address: 'SMR'
-      }
-    };
+  constructor(){
+    super();
+    this.state = {
+      monster: [
+        {
+          id: 1,
+          name: 'De Gozo',
+        },
+        {
+          id: 2,
+          name: 'Rogine',
+        },
+        {
+          id: 3,
+          name: 'Reniella',
+        },
+      ]
+    }
+  }
+  
   render() {
     return (  
     <div className="App"> 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      {/* <h1>{this.state.string} </h1> */}
-      <p>Hi my name is {this.state.age}</p>
-      <button onClick={()=> this.setState({age:21})}>Change Text</button>
-      </header>
+      {
+        this.state.monster.map(monster => <h1 key={monster.id}>{monster.name}</h1>)
+      }
     </div>  
     )
   }
+  // <p>Hi my name is {this.state.string} and my age is {this.state.age}</p>
+  // <button onClick={()=>{
+  //   this.setState({
+  //     string: 'Rogine',
+  //     age: this.state.age + 1,
+      
+  //   })
+  // }}>Change Text</button>
 }
+
+
 export default App;
 
 // function App() {
