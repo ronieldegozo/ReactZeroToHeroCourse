f7a15e5d4473d1b058f4c8f18db70713846285d6
Mc Roniel De Gozo <ronieldegozo@gmail.com>
2022-03-02 16:37:33 +0800

    Adding a search for text box

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      monster: []
+      monster: [],
+      search: ''
     }
   }
+
   componentDidMount(){
-    fetch('http://localhost:8080')
+    fetch('https://monster-api-project.herokuapp.com')
     .then(response => response.json())
 
       .then(users => this.setState({
@@ -20,11 +22,25 @@ class App extends Component {
 
   }
   render() {  
+
+    const filtredMonserData = this.state.monster.filter((monster) =>{
+      return monster.monster.toLowerCase().includes(this.state.search);
+    })  
+
     return (  
-    <div className="App"> 
+    <div className="App">   
+        <input className='Search' type='search' placeholder='Monster' onChange={(event) =>{ //FILTERED SEARCH
+
+          const search = event.target.value.toLowerCase();
+          this.setState(()=>{  
+            return {search}
+          })
+          console.log(search);
+        }} />
+
         <CardList name="Roniel">
           {
-            this.state.monster.map(monster => <h1 key={monster._id}>{monster.monster} {monster.description}</h1> )
+            filtredMonserData.map(monster => <h1 key={monster._id}>{monster.monster} {monster.description}</h1> )
           }
         </CardList>
     </div>  
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './card.list.styles.css';
 
 export const CardList = (props) =>{
-    console.log(props);
+    // console.log(props);
     return (
         <div className='card-list'>{props.children}</div>
     )
