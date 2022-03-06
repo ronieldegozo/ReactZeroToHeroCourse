abcd2b81c9a9dfe0b503f2078c43ee8d5f25b6ac
Mc Roniel De Gozo <ronieldegozo@gmail.com>
2022-03-06 20:23:10 +0800

    Using Usestate and Arrow function on javascript

--- src/App.js
+++ src/App.js
@@ -1,54 +1,79 @@
-import React, {Component} from 'react';
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import CardLists from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.components';
-class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      monster: [],
-      search: ''
-    }
-  }
-  //FETCH API
-  componentDidMount(){
-    fetch('https://monster-api-project.herokuapp.com/monsters')
-    .then(response => response.json())
 
-      .then(users => this.setState({
-        monster: users.monster
-      }))
-  
-  }
+const App = () =>{
 
-  //SEARCH FUNCTION
-    onSearchChange = (event) =>{ //FILTERED SEARCH
-      const search = event.target.value.toLocaleLowerCase();
-      this.setState(()=>{  
-        return {search}
-      })
-    }
 
-  render() {  
-    const { monster, search } = this.state;
-    const { onSearchChange } = this;  
-    const filteredMonster = monster.filter((monster)=>{
-      return monster.monster.toLocaleLowerCase().includes(search);
-    })
+  const [search, setSearch]= useState('') //Value and set value
+  console.log({search})
 
-    return (  
-    <div className="App">   
-    <h1 className='monster-header'>Monster Data Platform </h1>
+  const onSearchChange = (event) =>{ //FILTERED SEARCH
+      const searchFieldString = event.target.value.toLocaleLowerCase();
+      setSearch(searchFieldString);
+  };
+
+  return (
+      <div className="App">   
+          <h1 className='monster-header'>Monster Data Platform </h1>
         <SearchBox 
           onChangeHandlerss={onSearchChange} 
           placeholder='Search Here!' 
           className='monster-search-box'
         />
-        <CardLists monster={filteredMonster} />
+        {/* <CardLists monster={filteredMonster} /> */}
     </div>  
-    )
-  }
+  )
 }
+
+// class App extends Component {
+//   constructor(){
+//     super();
+//     this.state = {
+//       monster: [],
+//       search: ''
+//     }
+//   }
+//   //FETCH API
+//   componentDidMount(){
+//     fetch('https://monster-api-project.herokuapp.com/monsters')
+//     .then(response => response.json())
+
+//       .then(users => this.setState({
+//         monster: users.monster
+//       }))
+  
+//   }
+
+//   //SEARCH FUNCTION
+//     onSearchChange = (event) =>{ //FILTERED SEARCH
+//       const search = event.target.value.toLocaleLowerCase();
+//       this.setState(()=>{  
+//         return {search}
+//       })
+//     }
+
+//   render() {  
+//     const { monster, search } = this.state;
+//     const { onSearchChange } = this;  
+//     const filteredMonster = monster.filter((monster)=>{
+//       return monster.monster.toLocaleLowerCase().includes(search);
+//     })
+
+//     return (  
+//     <div className="App">   
+//     <h1 className='monster-header'>Monster Data Platform </h1>
+//         <SearchBox 
+//           onChangeHandlerss={onSearchChange} 
+//           placeholder='Search Here!' 
+//           className='monster-search-box'
+//         />
+//         <CardLists monster={filteredMonster} />
+//     </div>  
+//     )
+//   }
+// }
 export default App;
